35a79b9f076945c82d1169b028a5272ccae431b7
jbr284 <jbrosa2009@gmail.com>
2025-07-10 19:23:03 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -8,11 +8,13 @@ const URLS_TO_CACHE = [
   "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.21/jspdf.plugin.autotable.min.js"
 ];
+
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
+
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
