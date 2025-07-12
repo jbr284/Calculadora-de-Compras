c2c7d973cd76d6570684a5f3675a25f28d02547b
jbr284 <jbrosa2009@gmail.com>
2025-07-11 22:45:02 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,36 @@
-const CACHE_NAME = "calculadora-compras-v1";
-const URLS_TO_CACHE = [
-  "/Calculadora-de-Compras/",
-  "/Calculadora-de-Compras/index.html",
-  "/Calculadora-de-Compras/manifest.json",
-  "/Calculadora-de-Compras/icon-192.png",
-  "/Calculadora-de-Compras/icon-512.png",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.21/jspdf.plugin.autotable.min.js"
+const CACHE_NAME = 'agenda-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    }).catch(function(err) {
+      console.error("Erro ao adicionar arquivos ao cache:", err);
+    })
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => caches.match('./index.html'))
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
