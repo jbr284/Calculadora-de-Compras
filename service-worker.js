612c93b88d8dc5c05e45217ef18e20ea92340d13
jbr284 <jbrosa2009@gmail.com>
2025-11-27 02:20:25 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,21 @@
-const CACHE_NAME = 'agenda-cache-v1';
+// [CORREÇÃO 1] Nome do cache único para este app
+const CACHE_NAME = 'compras-cache-v1';
+
+// [CORREÇÃO 2] Adicionei as bibliotecas do PDF aqui para funcionarem offline
 const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon-192.png', // Certifique-se que estes arquivos existem na pasta
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.21/jspdf.plugin.autotable.min.js'
 ];
 
 self.addEventListener('install', function(event) {
+  // Forçar atualização imediata (como fizemos na Agenda)
+  self.skipWaiting();
+  
   event.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(FILES_TO_CACHE);
@@ -31,6 +39,8 @@ self.addEventListener('activate', function(event) {
       return Promise.all(
         keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       );
+    }).then(() => {
+      return self.clients.claim();
     })
   );
 });
